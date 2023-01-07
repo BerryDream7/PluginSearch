575b7a5b245a8baa631dcb57398d730d612557e1
BerryDream7 <alina-kud@mail.ru>
2023-01-07 19:48:14 +0300

    change test

--- src/test/java/ru/netology/statistic/StatisticsServicePlugTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServicePlugTest.java
@@ -17,4 +17,16 @@ public class StatisticsServicePlugTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxInTheMiddle() {
+        StatisticsServicePlug service = new StatisticsServicePlug();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 665, 11, 11, 12};
+        long expected = 665;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
