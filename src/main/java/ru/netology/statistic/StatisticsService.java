27a4470e4198e1568212a650ea7a46198c8f8787
BerryDream7 <alina-kud@mail.ru>
2023-01-08 00:06:09 +0300

    add Spotbugs

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,6 +1,6 @@
 package ru.netology.statistic;
 
-public class StatisticsServicePlug {
+public class StatisticsService {
     /**
      * Calculate index of max income
      *
--- src/test/java/ru/netology/statistic/StatisticsServicePlugTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServicePlugTest.java
@@ -8,7 +8,7 @@ public class StatisticsServicePlugTest {
 
     @Test
     void findMax() {
-        StatisticsServicePlug service = new StatisticsServicePlug();
+        StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
@@ -20,7 +20,7 @@ public class StatisticsServicePlugTest {
 
     @Test
     void findMaxInTheMiddle() {
-        StatisticsServicePlug service = new StatisticsServicePlug();
+        StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 665, 11, 11, 12};
         long expected = 665;
